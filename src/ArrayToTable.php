51676faa703a8a131ddfa936fdfe868f406b0d00
Nicolaas <github@sunnysideup.co.nz>
2022-06-13 14:11:50 +1200

    WIP

--- src/ArrayToTable.php
+++ src/ArrayToTable.php
@@ -6,69 +6,83 @@ namespace Sunnysideup\Vardump;
 class ArrayToTable
 {
 
-    public static function convert(array $array, $maxCols = 20, $maxRows = 200) : string
+    public static function convert(array $array, $maxCols = 20, $maxRows = 20) : string
     {
-        $html = '
-        <style>
-            .vardump-data-table td {padding: 2px;}
-        </style>
-        <table class="vardump-data-table" border="1" width="100%;">
-        ';
-
-        $html .= '<tr>';
-        $colCount = 0;
+        $maxRows = 9999999999;
+        $html = '';
+        $rowCount = 0;
         if (count($array)) {
-            $header = $array[0] ?? $array;
-            if(is_array($header)) {
+            $html = '
+            <style>
+                .vardump-data-table {
+                    width: 100%;
+                }
+                .vardump-data-table td,
+                .vardump-data-table th {
+                    padding: 2px;
+                    font-size: 10px;
+                    text-align: left;
+                }
+            </style>
+            <table class="vardump-data-table" border="1">
+            ';
+            if(self::isMultiDimensionalArray($array)) {
+                $header = $array[0] ?? $array;
+                $html .= '<tr>';
+                $colCount = 0;
                 foreach($header as $key => $value){
                     $colCount++;
-                    if($colCount > $maxCols) {
-                        $html .= '<th>...</th>';
-                    } else {
+                    if($colCount < $maxCols) {
                         $html .= '<th>' . htmlspecialchars($key) . '</th>';
+                    } else {
+                        $html .= '<th>...</th>';
                     }
                 }
                 $html .= '</tr>';
-
                 // data rows
-                $rowCount = 0;
-                $hasInnerArray = false;
-                $rows = '';
+
                 foreach( $array as $key => $value){
                     if($rowCount < $maxRows) {
                         $rowCount++;
                         $colCount = 0;
-                        if( is_array($value)) {
-                            $hasInnerArray = true;
-                            $rows .= '<tr>';
-                            foreach($value as $key2 => $value2){
-                                $colCount++;
-                                if($colCount > $maxCols) {
-                                    $rows .= '<td>...</td>';
-                                }
-                                $rows .= '<td>' . htmlspecialchars($value2) . '</td>';
+                        $html .= '<tr>';
+                        foreach($value as $key2 => $value2){
+                            $colCount++;
+                            if($colCount < $maxCols) {
+                                $html .= '<td>' . strip_tags($value2) . '</td>';
+                            } else {
+                                $html .= '<td>...</td>';
                             }
-                            $rows .= '</tr>';
-                        } else {
-                            $rows .= '<td>'.$value.'</td>';
                         }
+                        $html .= '</tr>';
                     }
                 }
-                if(! $hasInnerArray) {
-                    $rows = '<tr>'.$rows.'</tr>';
-                }
-                $html .= $rows.'</table>';
-                if($rowCount === $maxRows) {
-                    $html = '<p>not all rows shown</p>';
-                }
             } else {
-                $html .= print_r($array, 1);
+                foreach ( $array as $key => $value){
+                    if($rowCount < $maxRows) {
+                        $rowCount++;
+                        $html .= '
+                            <tr>
+                                <th>'.$key.'</th>
+                                <td>' . strip_tags($value) . '</td>
+                            </tr>';
+                    }
+                }
+            }
+            $html .= '</table>';
+            if($rowCount === $maxRows) {
+                $html .= '<p>not all rows shown</p>';
             }
         } else {
-            $html = '<p>no data available</p>';
+            $html .= '<p>no data available</p>';
         }
 
         return $html;
+    }
+
+    protected static function isMultiDimensionalArray(array $array) : bool
+    {
+        return (count($array) !== count($array, COUNT_RECURSIVE));
     }
 
 }
